89339ca4184754b6b7df16922ab4ef498fd78999
Jing Gong <gongjing@kth.se>
2015-01-20 14:58:37 +0100

    Added PowerFLOW

---
+++ software/PowerFLOW/beskow/4.4d/files/powerflow_run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l 
+
+#SBATCH -J mytest
+
+# The jobID it needs to wait completion 
+###SBATCH  --dependency=246589:2464.5
+
+# wall-clock time will be given to this job
+#SBATCH -t 1:59:00
+
+# Number of nodes to be allocated is (NPROCS/32)+1
+#SBATCH -N 2
+
+# Number of MPI processes per node (default 32)
+#SBATCH --ntasks-per-node=32
+
+### Output and error files
+#SBATCH -e error_file.e%J
+#SBATCH -o output_file.o%J
+
+###########################################
+
+# Number of cores for simulation 
+export NPROCS=32
+
+# Number of timesteps
+export NUM_TIMESTEPS=1000
+
+# Link to cray mpi library 5.6.5
+export EXA_ALT_XE6_ROOT=/opt/cray/mpt/5.6.5
+export LD_LIBRARY_PATH=/opt/cray/mpt/5.6.5/gni/mpich2-intel/120/lib:$LD_LIBRARY_PATH
+
+module add powerflow/4.4d
+
+# Not sure the environment variables still used for intel haswell
+export MALLOC_TRIM_THRESHOLD_=-1
+export MALLOC_MMAP_MAX_=0
+
+exawatcher > watcher_sim_$SLURM_JOBID.o &
+exaqsub -xe6_decomp -decompose -decomp_log decompose_$SLURM_JOBID.o -xe6 -foreground -simulate -sim_log simulator_$SLURM_JOBID.o -nprocs $NPROCS -num_timesteps $NUM_TIMESTEPS -simoption "-checkpoint 144.500" *.cdi
+
---
+++ software/PowerFLOW/general.rst
@@ -0,0 +1,28 @@
+:orphan:
+
+
+.. include:: include.inc
+
+
+How to use PowerFLOW on PDC machines
+=================================
+PowerFLOW is commercial software, and PDC does not have a license. All licenses to use PowerFLOW at PDC must be supplied by the users.  You can use the environment variable
+
+.. code-block:: bash
+
+ CDLMD_LICENSE_FILE
+
+to set to point to the correct license server.
+
+The PowerFLOW files can be accessed by loading the appropriate modules. To see which versions of PowerFLOW are available use the command
+
+.. code-block:: bash
+
+ module avail powerflow
+
+In order to use  at PDC you must provide your own license for the software. 
+
+http://www.exa.com
+
+.. include:: ../../disclaimer.rst
+
