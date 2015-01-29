8f3bfd9d21f405e37c72fde50eaecff94f6ed200
Radovan Bast <rbast@users.noreply.github.com>
2015-01-29 13:35:30 +0100

    cleanup by pylint

--- generate_index.py
+++ generate_index.py
@@ -1,8 +1,8 @@
 
-'''
+"""
 This script crawls through subdirectories of software/
 and generates include files
-'''
+"""
 
 import os
 import sys
@@ -18,25 +18,24 @@ def repeat_to_length(string_to_expand, length):
 
 #-------------------------------------------------------------------------------
 
-def get_divider_line(max_column_width, c, skip_first=False):
+def get_divider_line(max_column_width, char, skip_first=False):
 
     if skip_first:
-        s = ['|']
+        text = ['|']
     else:
-        s = ['+']
+        text = ['+']
     for i, width in enumerate(max_column_width):
         if i == 0 and skip_first:
-            s.append(repeat_to_length(' ', width + 2))
+            text.append(repeat_to_length(' ', width + 2))
         else:
-            s.append(repeat_to_length(c, width + 2))
-        s.append('+')
-    return ''.join(s)
+            text.append(repeat_to_length(char, width + 2))
+        text.append('+')
+    return ''.join(text)
 
 #-------------------------------------------------------------------------------
 
 def get_sphinx_table(table):
 
-    num_lines = len(table)
     num_columns = len(table[0])
 
     max_column_width = []
@@ -52,29 +51,29 @@ def get_sphinx_table(table):
             if width > max_column_width[i]:
                 max_column_width[i] = width
 
-    s = []
+    text = []
     for i, line in enumerate(table):
         skip_first = False
         if i == 0:
-            s.append(get_divider_line(max_column_width, '-'))
+            text.append(get_divider_line(max_column_width, '-'))
         elif i == 1:
-            s.append(get_divider_line(max_column_width, '='))
+            text.append(get_divider_line(max_column_width, '='))
         else:
             # do not repeat program name if it is in the line above
             skip_first = (table[i][0] == table[i-1][0])
-            s.append(get_divider_line(max_column_width, '-', skip_first))
-        s3 = ['|']
-        for j, w in enumerate(line):
+            text.append(get_divider_line(max_column_width, '-', skip_first))
+        text2 = ['|']
+        for j, word in enumerate(line):
             if j == 0 and skip_first:
-                z = ' '
+                word2 = ' '
             else:
-                z = w
-            s3.append(' %s%s ' % (z, repeat_to_length(' ', max_column_width[j] - len(z))))
-            s3.append('|')
-        s.append(''.join(s3))
-    s.append(get_divider_line(max_column_width, '-'))
+                word2 = word
+            text2.append(' %s%s ' % (word2, repeat_to_length(' ', max_column_width[j] - len(word2))))
+            text2.append('|')
+        text.append(''.join(text2))
+    text.append(get_divider_line(max_column_width, '-'))
 
-    return '\n'.join(s)
+    return '\n'.join(text)
 
 #-------------------------------------------------------------------------------
 
@@ -111,24 +110,24 @@ def generate_table(table, programs, version_d, systems, section, only_program=''
 
 def main():
 
-    SYSTEMS = ['Beskow', 'Lindgren', 'Povel', 'Ellen', 'Zorn']
+    systems = ['Beskow', 'Lindgren', 'Povel', 'Ellen', 'Zorn']
 
     software_path = os.path.join(os.getcwd(), 'software')
 
     # get list of all installed programs
     programs = []
     version_d = {}
-    for root, dirnames, filenames in os.walk(software_path):
-        for f in filenames:
-            if f == 'general.rst':
+    for root, _, filenames in os.walk(software_path):
+        for name in filenames:
+            if name == 'general.rst':
                 program = root.split('/')[-1]
                 programs.append(program)
                 version_d[program] = []
 
     # get list of all installed versions
-    for root, dirnames, filenames in os.walk(software_path):
-        for f in filenames:
-            if f == 'running.rst' or f == 'building.rst':
+    for root, _, filenames in os.walk(software_path):
+        for name in filenames:
+            if name == 'running.rst' or name == 'building.rst':
                 version = root.split('/')[-1]
                 program = root.split('/')[-3]
                 if version not in version_d[program]:
@@ -144,61 +143,61 @@ def main():
     # build include files which contain title and version
     for program in programs:
         for version in version_d[program]:
-            for system in SYSTEMS:
+            for system in systems:
                 for section in ['Running', 'Building']:
                     if os.path.isfile(os.path.join('software', program, system.lower(), version, '%s.rst' % section.lower())):
                         with open(os.path.join('software', program, system.lower(), version, '%s.inc' % section.lower()), 'w') as f_include:
-                            s = '%s %s %s on %s' % (section, program, version, system)
-                            f_include.write('%s\n' % s)
-                            f_include.write('%s\n' % repeat_to_length('=', len(s)))
+                            text = '%s %s %s on %s' % (section, program, version, system)
+                            f_include.write('%s\n' % text)
+                            f_include.write('%s\n' % repeat_to_length('=', len(text)))
 
     # here we remove Lindgren
     # we need it for the above until we also remove the files from the repo
-    SYSTEMS.remove('Lindgren')
+    systems.remove('Lindgren')
 
     top_line_program = []
     top_line_program.append('Version')
-    for system in SYSTEMS:
+    for system in systems:
         top_line_program.append(system)
 
     # this generates a version overview for each program separately
     for program in programs:
         with open(os.path.join('software', program, 'include.inc'), 'w') as f_program:
-            s = 'Running %s at PDC' % program
-            f_program.write('%s\n' % s)
-            f_program.write('%s\n' % repeat_to_length('=', len(s)))
+            title = 'Running %s at PDC' % program
+            f_program.write('%s\n' % title)
+            f_program.write('%s\n' % repeat_to_length('=', len(title)))
             table = []
             table.append(top_line_program)
-            table = generate_table(table, programs, version_d, SYSTEMS, 'running', only_program=program)
+            table = generate_table(table, programs, version_d, systems, 'running', only_program=program)
             f_program.write(get_sphinx_table(table))
 
     # generate main index file
-    with open('include.inc', 'w') as f:
+    with open('include.inc', 'w') as include_file:
 
         top_line = []
-        top_line.append('Software')
+        top_line.append('Soinclude_file')
         top_line.append('Version')
-        for system in SYSTEMS:
+        for system in systems:
             top_line.append(system)
 
-        f.write('\n\nSoftware at PDC\n')
-        f.write('===============\n')
+        include_file.write('\n\nSoftware at PDC\n')
+        include_file.write('===============\n')
 
-        f.write('\n\nRunning software\n')
-        f.write('----------------\n')
-
-        table = []
-        table.append(top_line)
-        table = generate_table(table, programs, version_d, SYSTEMS, 'running')
-        f.write(get_sphinx_table(table))
-
-        f.write('\n\n\nBuilding software\n')
-        f.write('-----------------\n\n')
+        include_file.write('\n\nRunning software\n')
+        include_file.write('----------------\n')
 
         table = []
         table.append(top_line)
-        table = generate_table(table, programs, version_d, SYSTEMS, 'building')
-        f.write(get_sphinx_table(table))
+        table = generate_table(table, programs, version_d, systems, 'running')
+        include_file.write(get_sphinx_table(table))
+
+        include_file.write('\n\n\nBuilding software\n')
+        include_file.write('-----------------\n\n')
+
+        table = []
+        table.append(top_line)
+        table = generate_table(table, programs, version_d, systems, 'building')
+        include_file.write(get_sphinx_table(table))
 
 #-------------------------------------------------------------------------------
 
