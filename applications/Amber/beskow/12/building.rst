a13a745b4bc965502cbd365fbd9d04ff2785dfd5
Radovan Bast <rbast@users.noreply.github.com>
2015-03-19 11:26:09 +0100

    add amber 12 building doc

---
+++ applications/Amber/beskow/12/building.rst
@@ -0,0 +1,18 @@
+The code has been installed using the following script:
+
+.. literalinclude:: files/install.sh
+    :language: bash
+
+The build contains the following patches::
+
+  Available AmberTools 13 patches:
+
+  update.1, update.2, update.3, update.4, update.5, update.6, update.7, update.8, update.9, update.10,
+  update.11, update.12, update.13, update.14, update.15, update.16, update.17, update.18, update.19, update.20,
+  update.21, update.22, update.23, update.24, update.25, update.26
+
+  Available Amber 12 patches:
+
+  bugfix.1, bugfix.2, bugfix.3, bugfix.4, bugfix.5, bugfix.6, bugfix.7, bugfix.8, bugfix.9.bz2_, bugfix.10,
+  bugfix.11, bugfix.12, bugfix.13, bugfix.14, bugfix.15, bugfix.16, bugfix.17, bugfix.18.bz2, bugfix.19, bugfix.20,
+  bugfix.21
---
+++ applications/Amber/beskow/12/files/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash --login
+
+#SBATCH -J build-amber
+#SBATCH -t 00:59:00
+#SBATCH -N 1
+#SBATCH -o stdout.txt
+#SBATCH -e stderr.txt
+
+# here we will install Amber 12 with AmberTools 13
+# for the installation we follow (the latter is for Amber 11):
+# http://jswails.wikidot.com/installing-amber12-and-ambertools-13
+# http://www.hector.ac.uk/support/documentation/software/amber/compiling-phase3.php
+
+# we load the following modules:
+module swap PrgEnv-cray PrgEnv-gnu
+module add cray-netcdf/4.3.2
+module add fftw/3.3.4.0
+
+# extract files and set amberhome:
+cd /pdc/vol/amber/12
+tar jxvf src/AmberTools13.tar.bz2
+tar jxvf src/Amber12.tar.bz2
+cd amber12
+export AMBERHOME=/pdc/vol/amber/12/amber12
+
+# we first build the mpi version
+# the code has to be built on a compute node otherwise it does not compile
+# we configure piping yes so that all patches get applied
+yes | ./configure -static -mpi -noX11 -nomtkpp -crayxt5 --with-netcdf /opt/cray/netcdf/4.3.2/gnu/49 gnu
+aprun -n 1 -d 32 make install -j32
+# then the serial build
+yes | ./configure -static      -noX11 -nomtkpp -crayxt5 --with-netcdf /opt/cray/netcdf/4.3.2/gnu/49 gnu
+aprun -n 1 -d 32 make install -j32
