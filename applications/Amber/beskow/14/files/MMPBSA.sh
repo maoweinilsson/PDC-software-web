d68ba174b688d2845d6795e9a89c63d3088b245d
Rossen Apostolov <rossen@kth.se>
2015-11-06 13:58:24 +0100

    Typo

--- applications/Amber/beskow/14/files/MMPBSA.sh
+++ applications/Amber/beskow/14/files/MMPBSA.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-python /pdc/vol/amber/14/amber12/bin/MMPBSA.py.MPI -O -i mmgbsa1.in -o MMGBSA1.dat -sp 2BEG_AZD_wi.prmtop -cp 2BEG_AZD.prmtop -rp 2BEG.prmtop -lp AZD.prmtop -y nvt.mdcrd >& MMPBSA.log
+python /pdc/vol/amber/14/amber14/bin/MMPBSA.py.MPI -O -i mmgbsa1.in -o MMGBSA1.dat -sp 2BEG_AZD_wi.prmtop -cp 2BEG_AZD.prmtop -rp 2BEG.prmtop -lp AZD.prmtop -y nvt.mdcrd >& MMPBSA.log
