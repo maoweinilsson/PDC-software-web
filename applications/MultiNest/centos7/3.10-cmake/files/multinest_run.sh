f8a15d90021e3546e5c77771200f5061a30268c9
Jing Gong <gongjing@kth.se>
2016-01-15 15:12:15 +0100

    Added new version to MultiNest

---
+++ applications/MultiNest/centos7/3.10-cmake/files/multinest_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# The name of the script is myjob
+#SBATCH -J myjob
+
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 1:00:00
+
+# Number of nodes
+#SBATCH --nodes=2
+
+# Output and error files
+#SBATCH -e error_file.e
+#SBATCH -o output_file.o
+
+# load module multinest
+module add multinest/3.10-cmake
+
+# Run multinest
+mpirun -np 48 eggboxC
+
