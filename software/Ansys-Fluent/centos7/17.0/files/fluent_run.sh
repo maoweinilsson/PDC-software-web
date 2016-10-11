55c6b8bd4a567653c18024829d3f969038550f91
Jing Gong <gongjing@kth.se>
2016-10-11 10:09:10 +0200

    Added Ansys v17.0 on Tegner

---
+++ software/Ansys-Fluent/centos7/17.0/files/fluent_run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+# The name of the script is myjob
+#SBATCH -J myjob
+
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 1:00:00
+
+# Number of nodes
+#SBATCH -N 2
+
+# Number of MPI processes per node 
+###SBATCH --ntasks-per-node=24
+
+#SBATCH -e error_file.e
+#SBATCH -o output_file.o
+
+# load module fluent v17.0
+module add fluent/17.0 
+
+# The Journal file
+JOURNALFILE=mycase.jou
+
+# Total number of Processors
+#NPROCS=24
+NTASKS=`echo $SLURM_TASKS_PER_NODE | cut -c1-2`
+NPROCS=`expr $SLURM_NNODES \* $NTASKS`
+
+if [ $SLURM_NNODES -eq 1 ]; then
+    # Single node with shared memory
+    fluent 3ddp -g -t $NPROCS -i $JOURNALFILE > fluent.log 
+else
+    # Multi-node
+    fluent 3ddp -g -slurm -t $NPROCS -mpi=pcmpi -pib.dapl -i $JOURNALFILE > fluent.log 
+fi
+
