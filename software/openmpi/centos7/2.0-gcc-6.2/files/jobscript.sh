46299f90c888ebcf9f8e6f97db182b4d2ed7f13a
Jing Gong <gongjing@kth.se>
2016-12-26 18:49:39 +0100

    Added openmpi 2.0 on centos7

---
+++ software/openmpi/centos7/2.0-gcc-6.2/files/jobscript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Set the allocation to be charged for this job
+# not required if you have set a default allocation
+
+# The name of the script is myjob
+#SBATCH -J myjob
+
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 1:00:00
+
+# set the project to be charged for this
+# should normally be of the format 2017-1 or 2017-16-1 or similar
+#SBATCH -A 2017-1
+
+# Number of nodes
+#SBATCH --nodes=2
+# Number of MPI processes per node (24 is recommended for most cases)
+# 48 is the default to allow the possibility of hyperthreading
+#SBATCH --ntasks-per-node=24
+# Number of MPI processes.
+
+#SBATCH -e error_file.e
+#SBATCH -o output_file.o
+
+# load the intel mpi modules
+module load gcc/6.2.0
+module load openmpi/2.0-gcc-6.2
+
+# Run the executable named myexe
+# and write the output into my_output_file
+mpirun -np 48 ./myexe > my_output_file 
+
