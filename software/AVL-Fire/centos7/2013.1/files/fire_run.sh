f9ce615ba2899f856ef4dbdbdb81ca91168aab83
Jing Gong <gongjing@kth.se>
2017-02-08 08:59:45 +0100

    Added new versions of AVL/fire on centos7

---
+++ software/AVL-Fire/centos7/2013.1/files/fire_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+# The name of the script is myjob
+#SBATCH -J myjob
+
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 01:00:00
+
+# set the project to be charged for this
+# should normally be of the format 2017-1 or 2017-17-1 or similar
+#SBATCH -A 2017-1
+
+# Number of nodes
+#SBATCH --nodes=2
+
+#SBATCH -e error_file.e
+#SBATCH -o output_file.o
+ 
+# Load the AVL fire module
+module load fire/2013.1
+
+# Set the number of cores 
+ncpus=48
+
+# Adapt the name of nodes for AVL Fire
+srun hostname  |  sort |  uniq > nodelist
+export hosts=`hostconvert_fire_tegner.pl nodelist`
+
+# Run the fire command
+fire_cmd -name=fire -no_file_locking -nomapping -mpi -cpu=$ncpus -hostlist=$hosts -project_dir=$PWD -case=Case -solver_vers=v2013
+
--- software/AVL-Fire/general.rst
+++ software/AVL-Fire/general.rst
@@ -6,6 +6,10 @@ How to use AVL/Fire on PDC machines
 ===================================
 AVL/Fire is commercial software and the users must provide their own license to the software. 
 
+To see which versions of AVL/Fire are installed on any of the machines at PDC log into the machine and type ::
+
+ module avail fire
+
 More details about the software, see
 
 http://www.avl.com
