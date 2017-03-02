7f6ca17cc6c63bfb7bc269f384bb09307b388b4b
Thor Wikfeldt <kthw@kth.se>
2017-03-02 14:30:09 +0100

    add matlab r2016b doc

---
+++ software/matlab/centos7/r2016b/files/run_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -A 201X-X-XX      
+#SBATCH -J myjob                              
+#SBATCH -t 0:05:00                                 
+#SBATCH --nodes=1                              
+#SBATCH -e error_file.e                             
+#SBATCH -o output_file.o
+
+module add matlab/r2016b
+# Run matlab taking your_matlab_program.m as input and show the output in the file
+# your_matlab_program.out. The input file must be in the directory where you submit this script. 
+# This is also where the output will be created.
+
+matlab -nojvm -nosplash -nodesktop -nodisplay < your_matlab_program.m > your_matlab_program.out
+
+# If you remove the part:
+# > your_matlab_program.out
+# the output will instead be shown in the file output_file.o specified above
---
+++ software/matlab/centos7/r2016b/files/run_manyserial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -A 201X-X-XX      
+#SBATCH -J myjob                              
+#SBATCH -t 0:05:00                                 
+#SBATCH --nodes=1                              
+#SBATCH -e error_file.e                             
+#SBATCH -o output_file.o
+
+module add matlab/r2016b
+# Run matlab for 24 individual programs serial_program_1.m, 
+# serial_program_2.m ... and print output in files logfile_1, logfile_2, ...
+# The input files must be in the directory where you submit this script. 
+# This is also where the output will be created.
+
+for i in {1..24} ; do
+    matlab -nojvm -nosplash -nodesktop -nodisplay < serial_program_${i}.m > logfile_$i &
+done
+# this wait command must be present since otherwise the job will terminate immediately
+wait  
+
