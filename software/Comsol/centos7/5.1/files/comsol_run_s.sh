5a100482c6bedf2586a3e52c63ade75e1f1282dc
Jing Gong <gongjing@kth.se>
2017-03-14 10:36:29 +0100

    Updates job scripts for comsol

--- software/Comsol/centos7/5.1/files/comsol_run_s.sh
+++ software/Comsol/centos7/5.1/files/comsol_run_s.sh
@@ -6,8 +6,16 @@
 # Only 1 hour wall-clock time will be given to this job
 #SBATCH -t 1:00:00
 
+# set the project to be charged for this
+# should normally be of the format 2017-1 or 2017-17-1 or similar
+#SBATCH -A 2017-1
+
 # Number of nodes
 #SBATCH --nodes=1
+
+# Number of MPI processes per node (24 is recommended for most cases)
+# 48 is the default to allow the possibility of hyperthreading
+#SBATCH --ntasks-per-node=24
 
 #SBATCH -e error_file.e
 #SBATCH -o output_file.o
